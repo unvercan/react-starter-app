ea40d3fd51868405f83a3164a93f9560ed46021d
Ünver Can Ünlü <unvercanunlu@gmail.com>
2020-02-11 00:00:18 +0300

    fifth commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+
+module.exports = {
+  rootDir: 'test',
+  testMatch: ['**/*.*(spec|test).{json,js,jsx}'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleDirectories: ['node_modules']
+};
